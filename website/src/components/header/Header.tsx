ca4392223c86ef8b237789ded0658b1e2dffac62
tom baldwin <100462392+tomrbaldwin@users.noreply.github.com>
2026-05-07 16:24:20 +0700

    setup react

---
+++ website/src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-contents">
+        <h2>Tom Baldwin</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
