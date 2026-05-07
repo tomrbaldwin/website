d2213883968d59a6bfce2b73f405c0b77ccbc50b
tom baldwin <100462392+tomrbaldwin@users.noreply.github.com>
2026-05-07 19:49:21 +0700

    add components

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 
 function App() {
   return (
-    <>
+    <div className="app-container">
       <Header />
-    </>
+      <main className="main-content">
+        {/* Your page content will go here */}
+      </main>
+      <Footer />
+    </div>
   );
 }
 
---
+++ website/src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import "./footer.css";
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Tom Baldwin. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
